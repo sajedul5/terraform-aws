fb831287b68061663189344012143494da8d225b
cloud.admin2 <cloud.admin2@brac.net>
2026-06-04 21:44:30 +0600

    terraform-provider

---
+++ terraform-provider/main.tf
@@ -0,0 +1,31 @@
+### Basic Provider Configuration
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+
+### Multiple Provider Versions
+
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.0"
+#     }
+#     random = {
+#       source  = "hashicorp/random"
+#       version = "~> 3.1"
+#     }
+#   }
+# }
