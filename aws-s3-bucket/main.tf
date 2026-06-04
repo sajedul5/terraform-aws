8214d43d3732323dc851c6170a59997b0748298a
cloud.admin2 <cloud.admin2@brac.net>
2026-06-04 22:15:18 +0600

    aws-s3-bucket

---
+++ aws-s3-bucket/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+    region = "us-east-1"
+}
+
+# Create a S3 bucket
+resource "aws_s3_bucket" "tf_test_baivab_bucket" {
+  bucket = "my-tf-test-bucket-101999999"
+
+  tags = {
+    Name        = "My bucket v-2.0"
+    Environment = "Dev"
+  }
+}
+
