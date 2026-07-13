95e852f4ee8f2df6e42d7e74b8f2d119ce8b3601
cloud.admin2 <cloud.admin2@brac.net>
2026-07-13 22:22:25 +0600

    terraform-functions

---
+++ terraform-functions/code/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+
+  backend "s3" {
+    bucket       = "dev-ops-terraform-state-files"
+    key          = "function-terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = "true"
+    encrypt      = true
+  }
+}
