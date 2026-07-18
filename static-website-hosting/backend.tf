39ec2b3897c3e367f1b2381cfe42b80abf57ffe4
cloud.admin2 <cloud.admin2@brac.net>
2026-07-18 13:17:20 +0600

    backend.tf file comment added

--- static-website-hosting/backend.tf
+++ static-website-hosting/backend.tf
@@ -1,7 +1,7 @@
 terraform {
 
   backend "s3" {
-    bucket       = "dev-ops-terraform-state-files"
+    bucket       = "dev-ops-terraform-state-files" # change this to your S3 bucket name
     key          = "static-website-hosting.tfstate"
     region       = "us-east-1"
     use_lockfile = "true"
