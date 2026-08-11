320823465556afdafae5b400476e780599589163
sajedul5 <sajedul.idb@gmail.com>
2026-08-11 19:40:24 +0600

    backend.tf & provider.tf added

--- terraform-custom-modules-for-eks/provider.tf
+++ terraform-custom-modules-for-eks/provider.tf
@@ -1,5 +1,5 @@
 # Provider Configuration
-# Using AWS Provider 5.x (Compatible with EKS module 20.x)
+# Using AWS Provider 6.x (Compatible with EKS module 20.x)
 
 terraform {
   required_version = ">= 1.3"
@@ -7,7 +7,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.0"
     }
   }
 }
@@ -18,7 +18,7 @@ provider "aws" {
   default_tags {
     tags = {
       ManagedBy = "Terraform"
-      Project   = "Day20-EKS-Cluster"
+      Project   = "EKS-Cluster"
     }
   }
 }
