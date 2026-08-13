70abf7fbb1d161d808f08f63a2a959755dd3453b
sajedul5 <sajedul.idb@gmail.com>
2026-08-13 11:58:27 +0600

    updated

--- 2-tier-architecture-setup-on-aws/backend.tf
+++ 2-tier-architecture-setup-on-aws/backend.tf
@@ -2,8 +2,8 @@ terraform {
 
   backend "s3" {
     bucket       = "s3-terraform-state-files-backend"
-    key          = "2-tier-architecture-rds-demo.tfstate"
-    region       = "us-east-1"
+    key          = "two-tier-architecture-rds-demo.tfstate"
+    region       = "us-east-2"
     use_lockfile = "true"
     encrypt      = true
   }
