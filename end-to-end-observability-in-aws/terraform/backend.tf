3201b8e67d913584966b12d195a7f9cb219b481a
sajedul5 <sajedul.idb@gmail.com>
2026-08-14 23:02:21 +0600

    backend.tf updated

--- end-to-end-observability-in-aws/terraform/backend.tf
+++ end-to-end-observability-in-aws/terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
   backend "s3" {
     bucket       = "s3-terraform-state-files-backend"
     key          = "end-to-end-observability.tfstate"
-    region       = "us-east-1"
+    region       = "us-east-2"
     use_lockfile = "true"
     encrypt      = true
   }
